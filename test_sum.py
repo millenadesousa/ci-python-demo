8bb78dee277310eb499df5d264fddc841b3125ad
millenadesousa <liskwspees@gmail.com>
2025-10-20 07:30:22 -0300

    Add unit tests for sum function

---
+++ test_sum.py
@@ -0,0 +1,11 @@
+from sum import sum
+
+def test_sum_positivos():
+    assert sum(1, 2) == 3
+
+def test_sum_negativos_e_zero():
+    assert sum(-1, 1) == 0
+
+def test_sum_zeros():
+    assert sum(0, 0) == 0
+  
